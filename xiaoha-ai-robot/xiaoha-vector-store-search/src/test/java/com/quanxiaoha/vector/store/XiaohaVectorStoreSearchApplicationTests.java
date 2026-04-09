7e66652d7b7ac68338532b0817f0732e91dfbd59
Withnoidea <2482454653@qq.com>
2026-04-09 16:52:30 +0800

    RAG
    Document vectorized storage and retrieval

---
+++ xiaoha-ai-robot/xiaoha-vector-store-search/src/test/java/com/quanxiaoha/vector/store/XiaohaVectorStoreSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.vector.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohaVectorStoreSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
