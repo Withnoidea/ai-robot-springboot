627123a6da6b8fc44ed031c37eb31541a0956f1a
Withnoidea <2482454653@qq.com>
2026-04-10 10:36:44 +0800

    RAG
    DeepSeek integrates RAG to enhance search: enabling interaction with PDFs

---
+++ xiaoha-ai-robot/xiaoha-vector-store-search/src/main/java/com/quanxiaoha/vector/store/config/ChatClientConfig.java
@@ -0,0 +1,26 @@
+package com.quanxiaoha.vector.store.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Withnoidea
+ * @Date: 2026/4/10 10:26
+ * @Version: v1.0.0
+ * @Description: ChatClient 配置
+ */
+@Configuration
+public class ChatClientConfig {
+    /**
+     * 初始化 ChatClient 客户端
+     * @param chatModel
+     * @return
+     */
+    @Bean
+    public ChatClient chatClient(DeepSeekChatModel chatModel) {
+        return ChatClient.builder(chatModel)
+                .build();
+    }
+}
